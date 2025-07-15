795ec76fd43a766b3992b68ffbfcb9ae3e75f9b3
nomen <justnomen@outlook.com>
2025-07-15 11:49:43 +0100

    small refactor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import PiAppClient from "@/components/PiAppClient";
 export default function Home() {
     return (
         <>
-            {process.env.NODE_ENV === "development" && <div className="absolute top-0 left-0 p-1">Testnet</div>}
+            {process.env.NODE_ENV !== "production" && <div className="absolute top-0 left-0 p-1">Testnet</div>}
             <PiAppClient />
         </>
     );

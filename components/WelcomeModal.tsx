d347b63ad2a1d1cc82b7ac39b250d5b11582b514
xmneax <xmneax@gmail.com>
2026-02-16 21:22:02 +0100

    Update welcome modal with correct workflow steps

--- components/WelcomeModal.tsx
+++ components/WelcomeModal.tsx
@@ -23,14 +23,16 @@ export default function WelcomeModal({ open, onClose }: WelcomeModalProps) {
 
                     <ol className="list-decimal list-inside space-y-3 pl-1">
                         <li>
+                            <span className="font-semibold text-gray-800">Login with Pi:</span> Connect your Pi Network account to verify ownership.
+                        </li>
+                        <li>
                             <span className="font-semibold text-gray-800">Enter Wallet Address:</span> Provide your Pi wallet address for verification.
                         </li>
                         <li>
                             <span className="font-semibold text-gray-800">Watch an Ad:</span> A short ad will play. Watching it fully helps support the ecosystem.
                         </li>
                         <li>
-                            <span className="font-semibold text-gray-800">Receive 0.01 Pi:</span> Once the ad is complete, we&#39;ll facilitate the 0.01 Pi transfer to your wallet, enabling your
-                            lockup release!
+                            <span className="font-semibold text-gray-800">Receive 0.01 Pi:</span> Once the ad is complete, we&#39;ll send 0.01 Pi to your wallet, enabling your lockup release!
                         </li>
                     </ol>
                 </div>
